2eda5cfe71146402cc1e51b250b2492b188db1bc
Lucas Xavier <lucas.dxavier@hotmail.com>
2024-03-01 15:20:12 +0000

    update-header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,20 @@
 <template>
   <header class="header">
-    {{ name }}
+    <img class="imgg" alt="Vue logo" src="../assets/logo.png">
+
+    <div class="txt">{{ name }}</div>
   </header>
 </template>
 
 <script>
+import { ref } from "vue";
   export default{
-    data(){
-      return{
-        name: 'Here is the header component'
+    name: 'TheHeader',
+    setup(){
+      const name = ref('Here is the Header Component');
+
+      return {
+        name,
       }
     }
   }
@@ -16,9 +22,27 @@
 
 <style>
   .header{
-    background-color: rgb(250, 100, 0);
-    padding: 40px;
+    background-color: rgb(208, 92, 15);
+    padding: 16px;
     color: #fff;
-
+    left: 0;
+    position: fixed;
+    width: 100%;
+    top: 0;
   }
+
+  .imgg{
+    width: 60px;
+    height: 60px;
+    padding: 7px;
+    border: 4px #000000;
+    border-radius: 50%;
+    box-shadow: 0 0 7px #000000;
+  }
+
+  .txt{
+    font-size: 20px;
+    font-weight: bold;
+  }
+
 </style>
