2b1b840782b2a048c1cc1d7d8cea6ecf20d343db
Lucas Xavier <lucas.dxavier@hotmail.com>
2024-03-07 11:37:49 +0000

    delete task done

--- src/components/ListTask.vue
+++ src/components/ListTask.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="task">
-    <h2>Task List</h2>
-      <input type="text"
-        v-model="txtTask"
-        placeholder="Enter task"
-        @keyup.enter="AddTask"
-      >
-      <button @click="AddTask">Add Task</button><br/>
+  <section class="task">
+    <div>
+      <h2>Task List</h2>
+        <input type="text"
+          v-model="txtTask"
+          placeholder="Enter task"
+          @keyup.enter="AddTask"
+        >
+        <button @click="AddTask">Add Task</button><br/>
+    </div>
 
-      <div v-if="ListTask.length != 0">
-        <h2>ALL ITEMS</h2>
-        <span
-          v-for="(list, index) in ListTask"
-          :key="index"
-          >
-        <input
-          v-if="list.length != 0"
-          type="checkbox"
-          v-model="CheckTask">
-        {{ list }}
-        </span>
-      </div>
+    <div v-if="ListTask.length != 0">
+      <h2>ALL ITEMS</h2>
+      <span
+        v-for="(list, index) in ListTask"
+        :key="index"
+        >
+      <input
+        v-if="list.length != 0"
+        type="checkbox"
+        v-model="CheckTask">
+          {{ list }}  - <button @click="delTask(index)">
+          X
+         </button> <br/>
+      </span>
+    </div>
 
-
-  </div>
+  </section>
 </template>
 
 <script setup>
@@ -33,21 +36,24 @@ const txtTask = ref("");
 const ListTask = ref([]);
 
 const AddTask = () => {
-  if (txtTask.value.trim() === "") {
-    alert("Please enter a task.");
-    txtTask.value = "";
-    return;
-  }
-  ListTask.value.forEach((item) => {
-    if (item === txtTask.value) {
-      alert("Task already exists");
-      txtTask.value = "";
-    }
-  });
+  // if (txtTask.value.trim() === "") {
+  //   alert("Please enter a task.");
+  //   txtTask.value = "";
+  //   return;
+  // }
+  // ListTask.value.forEach((item) => {
+  //   if (item === txtTask.value) {
+  //     alert("Task already exists");
+  //     txtTask.value = "";
+  //   }
+  // });
   ListTask.value.push(txtTask.value);
   txtTask.value = "";
 };
 
+const delTask = (index) => {
+  ListTask.value.splice(index, 1);
+};
 
 </script>
 
